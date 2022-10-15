ec7af4a18af1cd8f58f0833b59a1cec9d4297dcf
tmelanson17 <tmelanson17@gmail.com>
2022-10-14 23:39:29 -0700

    Readability changes / error parsing

--- type_matchup_reader.py
+++ type_matchup_reader.py
@@ -6,12 +6,19 @@ def create_type_and_matchups():
     type_names = []
     with open('type_matchup.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
-        for row in csv_reader:
+        first_row=True
+        for i, row in enumerate(csv_reader):
+            if first_row:
+                for typename in row[1:]:
+                    type_names.append(typename)
+                first_row=False
+                continue
+
             single_type_matchup = list()
             first_col=True
             for col in row:
                 if first_col:
-                    type_names.append(col.strip())
+                    assert i <= len(type_names) and col == type_names[i-1], "Type matchup rows/columns must match!"
                     first_col=False
                     continue
                 single_type_matchup.append(int(col))
@@ -20,8 +27,8 @@ def create_type_and_matchups():
         print(matchups)
         Type = IntEnum('Type', {t:i for t,i in zip(type_names, range(len(type_names)))})
         print([t for t in Type])
-        assert(Type.NULL in Type)
-        assert(len(matchups) == len(matchups[0]))
+        assert Type.NULL in Type, "Must include NULL Type!"
+        assert len(matchups) == len(matchups[0]), "Type matchup rows/columns must match!"
     return Type, matchups
 
 Type, EFFECTIVE_TABLE = create_type_and_matchups()
